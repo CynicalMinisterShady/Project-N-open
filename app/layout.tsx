d52ba9bad6972ac196c04b4b83b56357829f483d
jayesh-verma-code <jayeshvermaformalwork@gmail.com>
2025-10-06 03:59:52 +0530

    added vercel analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 // import {
 //   ClerkProvider,
 //   SignInButton,
@@ -115,6 +116,7 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
--- components/sampelvirzeonx/Marquee.tsx
+++ components/sampelvirzeonx/Marquee.tsx
@@ -5,7 +5,7 @@ import '@/styles/CustomMarquee.css'
 const MarqueeContent = () => {
     return (
         <div className="con-custom-marquee inline-block py-[2rem] relative whitespace-nowrap z-[90] text-[9vw] md:text-[7rem] font-[600]">
-            <h1 className="inline-block">EXPERINCE</h1>
+            <h1 className="inline-block">EXPERIENCE</h1>
             <div className="dot-ball inline-block w-[1.2rem] h-[1.2rem] md:w-[2rem] md:h-[2rem] lg:w-[4rem] lg:h-[4rem] rounded-[50%] bg-[#07797d] mb-1 md:mb-2 ml-[2rem] mr-[2rem]"></div>
             <h1 className="inline-block">NIRVEONX</h1>
             <div className="dot-ball inline-block w-[1.2rem] h-[1.2rem] md:w-[2rem] md:h-[2rem] lg:w-[4rem] lg:h-[4rem] rounded-[50%] bg-[#07797d] mb-1 md:mb-2 ml-[2rem] mr-[2rem]"></div>
